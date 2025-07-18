c054f345d6ec0ef42d6470cd2e70b8930498844c
LastBuff <jjjaaa1478@gmail.com>
2025-07-18 20:35:48 +0700

    add news

---
+++ sorce/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const carousel = document.getElementById("carousel");
+  const images = carousel.children;
+  const total = images.length;
+  let index = 0;
+
+  const updateSlide = () => {
+    carousel.style.transform = `translateX(-${index * 100}%)`;
+  };
+
+  document.getElementById("next").addEventListener("click", () => {
+    index = (index + 1) % total;
+    updateSlide();
+  });
+
+  document.getElementById("prev").addEventListener("click", () => {
+    index = (index - 1 + total) % total;
+    updateSlide();
+  });
+
+});
